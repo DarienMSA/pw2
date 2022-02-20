2f78b23a9935d1b2e20db91f99a0c812bc7186da
DarienMSA <sadarien@gmail.com>
2022-02-20 17:14:56 -0600

    Instalación finalizada

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const _BODYPARSER_ = require("body-parser");
+const _EXPRESS_ = require("express");
+const _APP_ = _EXPRESS_();
+const _PORT_ = 5000;
+require('./models/connection')
+
+/** requires de las rutas */
+const _USER_ROUTE_ = require('./routes/userRoutes');
+
+_APP_.use(_BODYPARSER_.json());
+
+_APP_.use('/api', _USER_ROUTE_);
+
+_APP_.listen(_PORT_, () => {
+    console.log("La aplicación está escuchando al puerto " + _PORT_);
+});
